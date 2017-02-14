42c55ef41538d3b79bd9d27274ac5dc91150a908
straga <vostraga@gmail.com>
2017-02-14 13:55:41 +0200

    update: descriptions

--- pricelist_tab/__openerp__.py
+++ pricelist_tab/__openerp__.py
@@ -1,28 +1,52 @@
 # -*- coding: utf-8 -*-
-
 {
-    "name" : "Product Price List Tab",
-    "version" : "8.0.2",
-    "author" : "Viktor Vorobjov",
-    'complexity': "easy",
-    "description" : """
+    "name": """Product Price List Tab""",
+    "summary": """Added support Price List Tab in product""",
+    "category": "sales",
+    "images": ['static/description/icon.png'],
+    "version": "1.0.0",
+
+    'description': """
+
+        =======================
+
     """,
-    "website" : "http://straga.github.io",
-    "depends" : ['product'],
-    "category" : "Stuff",
-    "sequence": 18,
-    "demo" : [],
-    "data" : [
+
+    "author": "Viktor Vorobjov",
+    "license": "LGPL-3",
+    "website": "https://straga.github.io",
+    "support": "vostraga@gmail.com",
+
+    "depends": [
+        "product",
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
+
         'res_extended_view.xml',
         'data/res_extended_level.xml',
         'wizard/mass_add_price_view.xml',
         'pricelist_tab_view.xml',
-        ],
-    'test': [],
-    'installable': True,
-    'application': True,
-    'active': False,
+
+              ],
+    "qweb": [],
+    "demo": [],
+
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+    "installable": True,
+    "auto_install": False,
+    "application": False,
 }
+
+
+
+
+
+
+
+
 
 
 
