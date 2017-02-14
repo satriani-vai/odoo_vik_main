29ed8b96fbc53a2d59b3fcc3f29d6e659f96d641
straga <vostraga@gmail.com>
2017-02-14 14:48:49 +0200

    update: descriptions

--- product_reorder/__openerp__.py
+++ product_reorder/__openerp__.py
@@ -1,24 +1,49 @@
 # -*- coding: utf-8 -*-
-
 {
-    "name" : "Product Reorder Rule",
-    "version" : "1.0.0",
-    "author" : "Viktor Vorobjov",
-    'complexity': "easy",
-    "description" : """
+    "name": """Product Reorder Rule""",
+    "summary": """Added support - Count Reorder Rule in Product in List View""",
+    "category": "product",
+    "images": ['static/description/icon.png'],
+    "version": "1.0.0",
+
+    "description": """
+
+        =======================
     """,
-    "website" : "http://straga.github.io",
-    "depends" : ['product','stock'],
-    "category" : "Stuff",
-    "sequence": 18,
-    "demo" : [],
-    "data" : [
+
+    "author": "Viktor Vorobjov",
+    "license": "LGPL-3",
+    "website": "https://straga.github.io",
+    "support": "vostraga@gmail.com",
+
+    "depends": [
+        "product","stock",
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
         'product_reorder_view.xml',
-        ],
-    'test': [],
-    'installable': True,
-    'application': True,
-    'active': False,
+              ],
+    "qweb": [],
+    "demo": [],
+
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+    "installable": True,
+    "auto_install": False,
+    "application": False,
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
