f945a16959e17bfc2fd39266021440159c5339be
straga <vostraga@gmail.com>
2017-02-14 15:03:52 +0200

    update: descriptions

--- product_retailprice/__openerp__.py
+++ product_retailprice/__openerp__.py
@@ -1,24 +1,51 @@
 # -*- coding: utf-8 -*-
-
 {
-    "name" : "Product Retail Price Field",
-    "version" : "8.0.2",
-    "author" : "Viktor Vorobjov",
-    'complexity': "easy",
-    "description" : """
+    "name": """Product Retail Price Field""",
+    "summary": """Added support - Product Retail Price Field in product View""",
+    "category": "product",
+    "images": ['static/description/icon.png'],
+    "version": "1.0.0",
+
+    "description": """
+
+        =======================
     """,
-    "website" : "http://www.prolv.net",
-    "depends" : ['product'],
-    "category" : "Stuff",
-    "sequence": 18,
-    "demo" : [],
-    "data" : [
-        'product_retailprice_view.xml',
-        ],
-    'test': [],
-    'installable': True,
-    'application': True,
-    'active': False,
+
+    "author": "Viktor Vorobjov",
+    "license": "LGPL-3",
+    "website": "https://straga.github.io",
+    "support": "vostraga@gmail.com",
+
+    "depends": [
+        "product","stock",
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
+         'product_retailprice_view.xml',
+              ],
+    "qweb": [],
+    "demo": [],
+
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+    "installable": True,
+    "auto_install": False,
+    "application": False,
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
