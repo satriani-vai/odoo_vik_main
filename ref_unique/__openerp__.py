ae55e49837b8e06ed1714fb7b539b0c711dc1162
straga <vostraga@gmail.com>
2017-02-14 16:11:47 +0200

    update: descriptions

--- ref_unique/__openerp__.py
+++ ref_unique/__openerp__.py
@@ -1,21 +1,52 @@
+# -*- coding: utf-8 -*-
 {
-    "name" : "Internal Ref Unique",
-    "version" : "8.0.0.1",
-    "author" : "Viktor Vorobjov",
-    'complexity': "easy",
-    "description" : """
-    Internal Ref Unique for products.
+    "name": """Internal Ref Unique""",
+    "summary": """Internal Ref Unique for products.""",
+    "category": "sale",
+    "images": ['static/description/icon.png'],
+    "version": "1.0.0",
+
+    "description": """
     """,
-    "website" : "http://www.prolv.net",
-    "depends" : ['base','product'],
-    "category" : "sale",
-    "sequence": 18,
-    "demo" : [],
-    "data" : [
-        ],
-    'test': [],
-    'installable': True,
-    'application': True,
-    'active': False,
+
+    "author": "Viktor Vorobjov",
+    "license": "LGPL-3",
+    "website": "https://straga.github.io",
+    "support": "vostraga@gmail.com",
+
+    "depends": [
+        "base", "product"
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
+
+              ],
+    "qweb": [],
+    "demo": [],
+
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+    "installable": True,
+    "auto_install": False,
+    "application": False,
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
